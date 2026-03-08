fd07578626d4c073cbdccbad2f0c2b02e7567ecc
jawasakher <jawasakher@example.com>
2026-03-08 23:43:51 +0300

    Add interactive navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useState } from "react";
+import Navbar from './components/Navbar'
 const App = () => {
+  const [theme, setTheme] = useState('light')
   return (
-    <div>
-      
+    <div className='dark:bg-black relative'>
+     <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
