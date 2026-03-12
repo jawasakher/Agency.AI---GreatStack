de1db958a994236a689183b8062ff4469832c6a6
jawasakher <jawasakher@example.com>
2026-03-13 00:00:20 +0300

    Changes

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ServiceCard = () => {
+  return (
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border
+    border-gray-200 dark:border-gray-700 shadow-gray-100
+    dark:shadow-white/10'>
+
+        <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r
+        from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0
+        transition-opacity duration-500 mix-blend-lighten opacity-70'>
+        
+
+        <div className=''>
+            
+        </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default ServiceCard
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
+import assets from '../assets/assets'
+import Title from './Title'
+
 const Services = () => {
+
+    const servicesData =[
+        {
+         title: 'Advertising',
+         description: 'We turn bold ideas into powerful digital solutions that connect, engage...',
+         icon:assets.ads_icon
+
+        },
+        {
+         title: 'Content marketing',
+         description: 'We help you execute your plan and deliver results.',
+         icon:assets.marketing_icon
+
+        },
+        {
+            title:'Content writing',
+            description:'We help you create amarketing strategy that delivers results.',
+            icon:assets.content_icon
+        },
+        {
+          title: 'Social media',
+          description: 'We help you build a strong social media presence that connects with your audience.',
+          icon: assets.social_icon
+        },
+    ] 
   return (
-    <div>
+    <div id='services' className=' relative flex flex-col items-center gap-7 px-4
+     sm:px-12 lg:px-24 xl:px-30 text-gray-700 dark:text-white'>
+
+
+        <img src={assets.bgImage2} alt="" className='absolute
+        -top-110 -left-70 -z-1 dark:hidden'/>
+        <Title title='How can we help?' desc='From strategy to
+         execution, we empower your brand to thrive in the digital landscape.'/>
       
     </div>
   )
