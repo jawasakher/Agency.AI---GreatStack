838e555eb43e3d66842c102919d622e5a8b00929
jawasakher <jawasakher@example.com>
2026-03-13 21:25:27 +0300

    dark theme

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,16 +4,16 @@ import React,{useState} from 'react'
 const ServiceCard = ({service, index}) => {
 
   const [position, setPosition] = React.useState({x:0, y:0})
-
+  const [visible, setVisible] = useState(false);
   return (
     <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border
     border-gray-200 dark:border-gray-700 shadow-gray-100
     dark:shadow-white/10'>
 
-        <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r
-        from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0
-        transition-opacity duration-500 mix-blend-lighten opacity-70' style={{ top:
-          position.y - 150, left: position.x -150}} />
+        <div className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r
+          from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0 
+          transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70'
+           : 'opacity-0'}`} style={{ top:position.y - 150, left: position.x - 150}} />
         
 
         <div className='flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5
